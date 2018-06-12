28f2171845d76b741b6c4f6d7a53da693f964c6e
chriszanf <chriszanf@gmail.com>
2018-06-12 11:52:22 +0100

    Update NonDefaultAdminActs.sh

--- NonDefaultAdminActs.sh
+++ NonDefaultAdminActs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 # Script to detect non default admin accounts
-
 # Lovingly stolen from https://jamfnation.jamfsoftware.com/discussion.html?id=6520
 
 groupmember=`dscl /Local/Default read /Groups/admin GroupMembership | tr ' ' '\n' | grep -Ev 'root|admin|GroupMembership:|admin' | tr '\n' ' '`
